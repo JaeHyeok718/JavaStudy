392d66069bfc3ce452e55a181db36047c3726e84
JaeHyeok Jang <sorento7718@naver.com>
2024-02-07 20:45:38 +0900

    문제풀이

--- JavaTest/src/com/java/study/programmers/level0/day4/Day4.java
+++ JavaTest/src/com/java/study/programmers/level0/day4/Day4.java
@@ -1,5 +1,46 @@
 package com.java.study.programmers.level0.day4;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Day4 {
 
+	public void method01() {
+		
+		
+		int[] arr= {0,1,2,4,3};
+		int[][] queries= {{0,4,2},{0,3,2},{0,2,2}};
+		ArrayList<Integer> result = new ArrayList<>();
+		
+		for(int i=0; i<queries.length; i++) {
+		    ArrayList<Integer> arr2 = new ArrayList<>();
+		    int firstIndex = queries[i][0];
+		    int secondIndex = queries[i][1];
+		    int thirdIndex = queries[i][2];
+		    
+		    for(int j=firstIndex; j<=secondIndex; j++) { // 수정된 범위
+		        if(arr[j] > thirdIndex) {
+		            arr2.add(arr[j]);
+		        }
+		    }
+		    if(arr2.isEmpty()) {
+		        result.add(-1); 
+		    } else {
+		        int min = Collections.min(arr2); 
+		        result.add(min);
+		    }
+		}
+		
+		int[] result2 = new int[result.size()];
+		for(int k =0; k<result.size();k++) {
+			result2[k]=result.get(k);
+		}
+		
+		System.out.println(Arrays.toString(result2));
+	}
+	
+	
+	
+	
 }
--- JavaTest/src/com/java/study/programmers/level0/day4/Run.java
+++ JavaTest/src/com/java/study/programmers/level0/day4/Run.java
@@ -4,7 +4,10 @@ public class Run {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+   Day4 d4= new Day4();
+		
+		d4.method01();
+		
 	}
 
 }
